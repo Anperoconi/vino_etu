9524e97a6944de1a9ce15fad0d489b6024b8ac02
Nicolas Fontaine <nikoniko.fontaine@gmail.com>
2023-03-06 13:08:15 -0500

    ajout de dataconf

---
+++ dataconf.php
@@ -0,0 +1,9 @@
+<?php
+	// Remplir et renommer dataconf.php
+	define("BASEURL", "main.js");
+
+	define('HOST', 'localhost');
+	define('USER', 'root');
+	define('PASSWORD', '');
+	define('DATABASE', 'vinodb');
+?>
